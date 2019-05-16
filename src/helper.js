61fbb2708d328a51d658ee2fb2152b33da96c73f
Luiza <luitays@mail.ru>
2019-05-16 22:52:06 +0300

    for deploy

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
 export const server = {
-    baseURL: 'http://localhost:3000'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://customer-list-api.herokuapp.com' : 'http://localhost:3000'
 }
