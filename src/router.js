44fe750b23c89728022969226f20083b3e019ef5
Luiza <luitays@mail.ru>
2019-05-17 21:35:44 +0300

    fix routes

--- src/router.js
+++ src/router.js
@@ -7,12 +7,22 @@ import CreateComponent from '@/components/customer/Create'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', redirect: { name: 'home' } },
-    { path: '/home', name: 'home', component: HomeComponent },
-    { path: '/create', name: 'Create', component: CreateComponent },
-    { path: '/edit/:id', name: 'Edit', component: EditComponent }
-  ]
+      {
+          path: '/',
+          name: 'home',
+          component: HomeComponent
+      },
+      {
+          path: '/create',
+          name: 'Create',
+          component: CreateComponent
+      },
+      {
+          path: '/edit/:id',
+          name: 'Edit',
+          component: EditComponent
+      }
+    ]
 })
